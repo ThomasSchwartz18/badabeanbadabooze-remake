8df25655a5d5e01c3c89b30522cb37c05ff8d88e
ThomasSchwartzz <thomas5818id@gmail.com>
2022-11-09 13:42:25 -0500

    Added - scrolling effect for column2 and corrected spinning effect for column3-bottom

--- resources/back-end/spinner-clockwise.js
+++ resources/back-end/spinner-clockwise.js
@@ -5,16 +5,8 @@ text.innerHTML = text.innerText.split("").map(
     `<span style="transform:rotate(${i*14}deg">${char}</span>`
 ).join("")
 
-// const text = document.querySelector('.spinner-counter-clockwise p');
-// text.innerHTML = text.innerText.split("").map(
-//     (char, i) =>
-//     `<span style="transform:rotate(${i*9.5}deg">${char}</span>`
-// ).join("")
-
-// const text = document.querySelector('.spinner-clockwise p', '.spinner-counter-clockwise p');
-// text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
-
-// const element = document.querySelector('.spinner-counter-clockwise p span');
-// for (let i = 0; i < element.clientHeight; i++) {
-//     element[i].style.transform = "rotate("+i*18+"deg)"
-// }
+const text2 = document.querySelector('.spinner-counter-clockwise p');
+text2.innerHTML = text2.innerText.split("").map(
+    (char, i) =>
+    `<span style="transform:rotate(${i*13}deg">${char}</span>`
+).join("")
